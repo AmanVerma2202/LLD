fc379468a18297aadb907f077eb0d7559a47c8b0
Aman Verma <aka.amanverma@gmail.com>
2026-07-16 14:38:04 +0530

    good file updated

---
+++ SRP/good.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// SRP-compliant design: each class has one clear responsibility.
+class User {
+    string name;
+    int age;
+    string email;
+
+public:
+    User(string name, int age, string email)
+        : name(move(name)), age(age), email(move(email)) {}
+
+    string getName() const { return name; }
+    int getAge() const { return age; }
+    string getEmail() const { return email; }
+};
+
+class UserProfile {
+public:
+    static void printInfo(const User& user) {
+        cout << "User name is " << user.getName() << "\n";
+    }
+};
+
+class UserValidator {
+public:
+    static bool isAdult(const User& user) {
+        return user.getAge() > 18;
+    }
+};
+
+class UserRepository {
+public:
+    static void saveToDatabase(const User& user) {
+        cout << "Saved user '" << user.getName() << "' to db!\n";
+    }
+
+    static void deleteFromDatabase(const User& user) {
+        cout << "Deleted user '" << user.getName() << "' from db!\n";
+    }
+};
+
+int main() {
+    User user("aman", 21, "aman@gmail.com");
+
+    UserProfile::printInfo(user);
+    cout << "Is adult? " << (UserValidator::isAdult(user) ? "yes" : "no") << "\n";
+
+    UserRepository::saveToDatabase(user);
+    UserRepository::deleteFromDatabase(user);
+}
